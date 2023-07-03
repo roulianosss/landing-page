6f2de9b4f7a444665b3fdc4e0c2371b4028b91cf
Roulianosss <julien.ortscheid@gmail.com>
2023-07-03 14:32:46 +0200

    responsive homepage

--- src/components/shared/header/header.tsx
+++ src/components/shared/header/header.tsx
@@ -1,13 +1,30 @@
+'use client'
+
 import styles from "./header.module.css";
 import HeaderLink from "./header-components/header-link";
 import NojykLogo from "../nojykLogo";
 import ConnectBtn from "./header-components/connect-btn";
+import Image from "next/image";
+import { useState } from "react";
 
-const header = () => {
+const Header = () => {
+
+  const [isOpenMenu, setIsOpenMenu] = useState(false)
+
   return (
-    <div className={styles.globalContainer}>
+    <div className={styles.globalContainer} style={{maxHeight: isOpenMenu ? '600px' : '60px'}}>
       <div className={styles.logoAndLinksContainer}>
-        <NojykLogo size={130} />
+        <div className={styles.logoContainer}>
+          <Image
+            src="./assets/pictogram/hamburger.svg"
+            height={40}
+            width={40}
+            alt="menu icon"
+            className={styles.menuIcon}
+            onClick={() => setIsOpenMenu(!isOpenMenu)}
+          />
+          <NojykLogo size={130} />
+        </div>
         <div className={styles.linksContainer}>
           <HeaderLink url="./" title="Accueil" />
           <HeaderLink url="./livreur" title="Devenez livreur" />
@@ -22,4 +39,4 @@ const header = () => {
   );
 };
 
-export default header;
+export default Header;
